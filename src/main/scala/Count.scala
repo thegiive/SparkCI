fba177c61c20b480ea63cfdaf4314b9b811510fd
Wisely Chen <thegiive@gmail.com>
2013-08-20 23:03:53 -0700

    Solved the unitest address already in use

--- src/main/scala/Count.scala
+++ src/main/scala/Count.scala
@@ -8,8 +8,6 @@ object Count {
   def countA(sc: SparkContext , logFile: String) : Long = {
     val logRDD = sc.textFile(logFile, 2).cache()
     val numAs = logRDD.filter(line => line.contains("a")).count()
-    //val numBs = logData.filter(line => line.contains("b")).count()
-    //println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     return numAs 
   }
 }
--- src/test/scala/hello/Lala.scala
+++ src/test/scala/hello/Lala.scala
@@ -3,13 +3,35 @@ import scala.collection.mutable.Stack
 import spark.SparkContext
 import SparkContext._
 import hello.Count
+import org.scalatest.BeforeAndAfter
 
-class Lala extends FunSuite {
+class Lala extends FunSuite  with BeforeAndAfter {
+	
+	var sc: SparkContext = _
+
+    before {
+//println( "############start###############" ) 
+    sc = new SparkContext( "local[4]" , "Simple Job" ) 
+  }
+
+
   test("count ") {
     val logFile = "src/test/resources/syslog"
-    val sc = new SparkContext( "local" , "Simple Job" ) 
     val i = Count.countA(sc , logFile)
     assert( i == 1039 )
   }
+
+  test("count 2 ") {
+    val logFile = "src/test/resources/syslog"
+    val i = Count.countA(sc , logFile)
+    assert( i == 1039 )
+  }
+
+  after{
+//println( "############end###############" ) 
+        sc.stop
+        sc = null
+        System.clearProperty("spark.driver.port")
+}
 }
 
