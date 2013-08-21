785b9da9732e1a9b2ad72412257d99352319990b
Wisely Chen <thegiive@gmail.com>
2013-08-20 22:03:33 -0700

    add bad test condition

--- src/test/scala/hello/Lala.scala
+++ src/test/scala/hello/Lala.scala
@@ -7,14 +7,14 @@ class Lala extends FunSuite {
 
   test("pop is invoked on a non-empty stack") {
     val logFile = "/var/log/syslog" // Should be some file on your system
-//    val sc = new SparkContext("local", "Simple Job", "$YOUR_SPARK_HOME",
- //     List("target/scala-2.9.3/simple-project_2.9.3-1.0.jar"))
-    val sc = new SparkContext( "local" , "Simple Job" ) 
+    val sc = new SparkContext("local", "Simple Job", "$YOUR_SPARK_HOME",
+      List("target/scala-2.9.3/simple-project_2.9.3-1.0.jar"))
+//    val sc = new SparkContext( "local" , "Simple Job" ) 
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
-//    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
-	assert( numAs == 1025 )
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+	assert( numAs >  1 )
   }
 
 }
